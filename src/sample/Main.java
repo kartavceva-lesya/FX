67c380d0ceab91464ea6bfde7e05393b931cc194
kartavceva-lesya <kartavceva-lesya@yandex.ru>
2014-05-30 11:18:54 +0400

    график и гисторграмма (не по сигналу)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedAreaChart;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -36,9 +39,7 @@ public class Main extends Application {
         }
     }
 
-    public Parent createContent() {
-
-
+    private Parent emg() {
         List<Double> list = readFile("D://EMG/healthy_10s2_value.txt");
         ObservableList<StackedAreaChart.Data> dataList = FXCollections.observableArrayList();
         for (int i = 0; i < 100; i++) {
@@ -52,9 +53,38 @@ public class Main extends Application {
         return new StackedAreaChart(xAxis, yAxis, areaChartData);
     }
 
+    public Parent gistogramma() {
+        String[] years = {"2007", "2008", "2009"};
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setCategories(FXCollections.<String>observableArrayList(years));
+        NumberAxis yAxis = new NumberAxis("Units Sold", 0.0d, 3000.0d, 1000.0d);
+        ObservableList<BarChart.Series> barChartData = FXCollections.observableArrayList(
+                new BarChart.Series("Apples", FXCollections.observableArrayList(
+                        new BarChart.Data(years[0], 567d),
+                        new BarChart.Data(years[1], 1292d),
+                        new BarChart.Data(years[2], 1292d)
+                )),
+                new BarChart.Series("Lemons", FXCollections.observableArrayList(
+                        new BarChart.Data(years[0], 956),
+                        new BarChart.Data(years[1], 1665),
+                        new BarChart.Data(years[2], 2559)
+                )),
+                new BarChart.Series("Oranges", FXCollections.observableArrayList(
+                        new BarChart.Data(years[0], 1154),
+                        new BarChart.Data(years[1], 1927),
+                        new BarChart.Data(years[2], 2774)
+                ))
+        );
+        return new BarChart(xAxis, yAxis, barChartData, 25.0d);
+    }
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(createContent()));
+        HBox box = new HBox(5);
+        box.getChildren().add(emg());
+        box.getChildren().add(gistogramma());
+        primaryStage.setScene(new Scene(box));
         primaryStage.show();
     }
 
