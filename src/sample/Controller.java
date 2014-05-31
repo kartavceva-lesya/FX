e5e417bed68c1d7ee7f294f7fc0ca85e1d1a65e4
kartavceva-lesya <kartavceva-lesya@yandex.ru>
2014-05-31 11:51:27 +0400

    один график и одна гистрограмма

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,9 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.StackedAreaChart;
+import javafx.scene.Parent;
+import javafx.scene.chart.*;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
@@ -16,6 +15,8 @@ import java.util.ResourceBundle;
 public final class Controller implements Initializable {
     @FXML
     private HBox graphBox;
+    @FXML
+    private HBox barChartBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -27,11 +28,13 @@ public final class Controller implements Initializable {
         List<Integer> eDifference = Utils.extremaDifference(extrema);
         List<Double> amplitude = Utils.indexAmplitude(extrema, list);
         int[] barGraph = Utils.barGraph(eDifference);
+
+        barChartBox.getChildren().add(createBarGraph(barGraph));
         int[] barGraph1 = Utils.barGraph(amplitude);
     }
 
     @SuppressWarnings("unchecked")
-    private AreaChart createMyoGraph(List<Double> list) {
+    private Parent createMyoGraph(List<Double> list) {
         ObservableList<StackedAreaChart.Data> dataList = FXCollections.observableArrayList();
         for (int i = 0; i < 100; i++) {
             dataList.add(new StackedAreaChart.Data(i, list.get(i)));
@@ -41,5 +44,28 @@ public final class Controller implements Initializable {
         NumberAxis xAxis = new NumberAxis("X Values", 0.0, 100.0, 2.0d);
         NumberAxis yAxis = new NumberAxis("Y Values", -0.2d, 0.2d, 0.01d);
         return new AreaChart(xAxis, yAxis, areaChartData);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Parent createBarGraph(int[] data) {
+        String[] interval = {"0 - 1", "1 - 2", "2 - 3", "3 - 4", "4 - 5"};
+        CategoryAxis xAxis = new CategoryAxis(FXCollections.observableArrayList(interval));
+        NumberAxis yAxis = new NumberAxis("Count", 0.0d, 3000.0d, 1000.0d);
+
+        ObservableList list = FXCollections.observableArrayList();
+        for (int i = 0; i < interval.length; i++) {
+            list.add(new BarChart.Data(interval[i], data[i]));
+        }
+
+        ObservableList<BarChart.Series> barChartData = FXCollections.observableArrayList(
+                new BarChart.Series("Base", FXCollections.observableArrayList(
+                        new BarChart.Data(interval[0], 567d),
+                        new BarChart.Data(interval[1], 1292d),
+                        new BarChart.Data(interval[2], 1292d),
+                        new BarChart.Data(interval[3], 1292d)
+                )),
+                new BarChart.Series("Sample", list)
+        );
+        return new BarChart(xAxis, yAxis, barChartData, 25.0d);
     }
 }
