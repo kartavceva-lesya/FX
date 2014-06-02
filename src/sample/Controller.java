81e8c786f9bd91955b28b80f215b94d10f5b22c4
kartavceva-lesya <kartavceva-lesya@yandex.ru>
2014-06-02 08:53:16 +0400

    один график и ве гистрограмма(значения выводятся по одному парамеры разные - по другому - одинаковые) вставлен код кнопки(раскрывающийся список)  но она не отображается

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,16 @@
 package sample;
 
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.*;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.List;
@@ -17,6 +21,8 @@ public final class Controller implements Initializable {
     private HBox graphBox;
     @FXML
     private HBox barChartBox;
+    @FXML
+    private HBox ChoiceBoxApp;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,8 +35,11 @@ public final class Controller implements Initializable {
         List<Double> amplitude = Utils.indexAmplitude(extrema, list);
         int[] barGraph = Utils.barGraph(eDifference);
 
-        barChartBox.getChildren().add(createBarGraph(barGraph));
+        barChartBox.getChildren().add(createBarGraph1(barGraph));
         int[] barGraph1 = Utils.barGraph(amplitude);
+
+        barChartBox.getChildren().add(createBarGraph2(barGraph));
+        int[] barGraph2 = Utils.barGraph(eDifference);
     }
 
     @SuppressWarnings("unchecked")
@@ -47,8 +56,8 @@ public final class Controller implements Initializable {
     }
 
     @SuppressWarnings("unchecked")
-    private Parent createBarGraph(int[] data) {
-        String[] interval = {"0 - 1", "1 - 2", "2 - 3", "3 - 4", "4 - 5"};
+    private Parent createBarGraph1(int[] data) {
+        String[] interval = {"0 - 1", "1 - 2", "2 - 3", "3 - 4", "4 - 5", "5 - 6", "6 - 7", "7 - 8", "8 - 9", "9 - 10"};
         CategoryAxis xAxis = new CategoryAxis(FXCollections.observableArrayList(interval));
         NumberAxis yAxis = new NumberAxis("Count", 0.0d, 3000.0d, 1000.0d);
 
@@ -59,13 +68,66 @@ public final class Controller implements Initializable {
 
         ObservableList<BarChart.Series> barChartData = FXCollections.observableArrayList(
                 new BarChart.Series("Base", FXCollections.observableArrayList(
-                        new BarChart.Data(interval[0], 567d),
-                        new BarChart.Data(interval[1], 1292d),
-                        new BarChart.Data(interval[2], 1292d),
-                        new BarChart.Data(interval[3], 1292d)
+                        new BarChart.Data(interval[0], 1393d),
+                        new BarChart.Data(interval[1], 2788d),
+                        new BarChart.Data(interval[2], 542d),
+                        new BarChart.Data(interval[3], 88d),
+                        new BarChart.Data(interval[4], 14d),
+                        new BarChart.Data(interval[5], 4d),
+                        new BarChart.Data(interval[6], 1d),
+                        new BarChart.Data(interval[7], 1d),
+                        new BarChart.Data(interval[8], 0d),
+                        new BarChart.Data(interval[9], 1d)
                 )),
                 new BarChart.Series("Sample", list)
         );
         return new BarChart(xAxis, yAxis, barChartData, 25.0d);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Parent createBarGraph2(int[] data) {
+        String[] interval = {"0 - 1", "1 - 2", "2 - 3", "3 - 4", "4 - 5", "5 - 6", "6 - 7", "7 - 8", "8 - 9", "9 - 10"};
+        CategoryAxis xAxis = new CategoryAxis(FXCollections.observableArrayList(interval));
+        NumberAxis yAxis = new NumberAxis("Count", 0.0d, 3000.0d, 1000.0d);
+
+        ObservableList list = FXCollections.observableArrayList();
+        for (int i = 0; i < interval.length; i++) {
+            list.add(new BarChart.Data(interval[i], data[i]));
+        }
+
+        ObservableList<BarChart.Series> barChartData = FXCollections.observableArrayList(
+                new BarChart.Series("Base", FXCollections.observableArrayList(
+                        new BarChart.Data(interval[0], 3d),
+                        new BarChart.Data(interval[1], 50d),
+                        new BarChart.Data(interval[2], 793d),
+                        new BarChart.Data(interval[3], 3617d),
+                        new BarChart.Data(interval[4], 314d),
+                        new BarChart.Data(interval[5], 43d),
+                        new BarChart.Data(interval[6], 5d),
+                        new BarChart.Data(interval[7], 3d),
+                        new BarChart.Data(interval[8], 3d),
+                        new BarChart.Data(interval[9], 1d)
+                )),
+                new BarChart.Series("Sample", list)
+        );
+        return new BarChart(xAxis, yAxis, barChartData, 25.0d);
+    }
+
+    @SuppressWarnings("unchecked")
+    public class ChoiceBoxApp extends Application {
+
+        public Parent createContent() {
+            ChoiceBox cb = new ChoiceBox();
+            cb.getItems().addAll("Dog", "Cat", "Horse");
+            cb.getSelectionModel().selectFirst();
+            return cb;
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            primaryStage.setScene(new Scene(createContent()));
+            primaryStage.show();
+        }
+
     }
 }
